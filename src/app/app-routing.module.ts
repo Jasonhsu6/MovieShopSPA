c0556b48b20c4d14bc54a56ebf7d127366524669
Jasonhsu6 <35356776+Jasonhsu6@users.noreply.github.com>
2020-12-09 23:36:05 -0500

    MovieShopSPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "movies/genres/:id", 
+    component: MoviesComponent,
+  },
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "movies/:id",
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
